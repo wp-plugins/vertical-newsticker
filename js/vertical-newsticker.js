550cf8d7e3bac7ebf3230ded72c3699d52b5f2e6
clearcrest <clearcrest@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-21 07:00:05 +0000

    drop the plugin
    git-svn-id: https://plugins.svn.wordpress.org/vertical-newsticker/trunk@1140240 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/vertical-newsticker.js
+++
@@ -1,8 +0,0 @@
-/**
-* 	Vertical Newsticker
-*
-*	@author	info@clearcrest.net
-*/
-var initDelay=600,scrollSpeed=40,marqueeSpeed=1,pauseIt=1,copySpeed=marqueeSpeed,pauseSpeed=0==pauseIt?copySpeed:0,actualHeight="";function scrollMarquee(){parseInt(crossMarquee.style.top)>-1*actualHeight+8?crossMarquee.style.top=parseInt(crossMarquee.style.top)-copySpeed+"px":crossMarquee.style.top=parseInt(marqueeHeight)+8+"px"}
-function initMarquee(){scrollSpeed=document.getElementById("clearcrest-vertical-newsticker-wrapper").getAttribute("v");crossMarquee=document.getElementById("clearcrest-vertical-newsticker");crossMarquee.style.top=0;marqueeHeight=document.getElementById("clearcrest-vertical-newsticker-wrapper").offsetHeight;actualHeight=crossMarquee.offsetHeight;window.opera||-1!=navigator.userAgent.indexOf("Netscape/7")?(crossMarquee.style.height=marqueeHeight+"px",crossMarquee.style.overflow="scroll"):setTimeout('leftTime=setInterval("scrollMarquee()",'+
-scrollSpeed+")",initDelay)}window.addEventListener?window.addEventListener("load",initMarquee,!1):window.attachEvent?window.attachEvent("onload",initMarquee):document.getElementById&&(window.onload=initMarquee);
